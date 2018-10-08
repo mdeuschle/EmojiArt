9a8f18c31163f3ecdf82dcff7521fa532dcfb321
Matt Deuschle <mdeuschle@gmail.com>
2018-10-07 20:10:27 -0700

    set up project

---
+++ EmojiArt/Controller/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  EmojiArt
+//
+//  Created by Matt Deuschle on 10/7/18.
+//  Copyright © 2018 Matt Deuschle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
